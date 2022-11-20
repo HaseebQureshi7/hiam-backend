cd55b6f0b4318afcc88672943b36fde53310e3d0
Haseeb Qureshi <38492875+HaseebQureshi7@users.noreply.github.com>
2022-11-20 16:35:37 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Signup, MakeUserProfile, ViewUserProfile, EditUserProfile, CheckUserNameAvailability, MakeUserExperience, ViewUserExperience, EditUserExperience, MakeUserProject, ViewUserProject, EditUserProject, MakeUserSkill, ViewUserSkill, EditUserSkill, MakeUserCertificate, ViewUserCertificate, EditUserCertificate, MakeUserLink, ViewUserLink, EditUserLink, SearchUsers, NewUsers, PeopleLikeYou, UpdateProfilePicture, GetUsername, UpdateProjectImage
+from .views import Signup, MakeUserProfile, ViewUserProfile, EditUserProfile, CheckUserNameAvailability, MakeUserExperience, ViewUserExperience, EditUserExperience, MakeUserProject, ViewUserProject, EditUserProject, MakeUserSkill, ViewUserSkill, EditUserSkill, MakeUserCertificate, ViewUserCertificate, EditUserCertificate, MakeUserLink, ViewUserLink, EditUserLink, SearchUsers, NewUsers, PeopleLikeYou, UpdateProfilePicture, GetUsername, UpdateProjectImage, ViewSingleExperience, ViewSingleProject, ViewSingleCertificate, DeleteUserExperience, DeleteUserProject, DeleteUserSkill, DeleteUserCertificate
 
 urlpatterns = [
     path('signup', Signup),
@@ -13,20 +13,29 @@ urlpatterns = [
 
     path('makeuserexperience', MakeUserExperience),
     path('viewuserexperience/<int:id>', ViewUserExperience),
+    path('viewsingleexperience/<int:id>', ViewSingleExperience),
     path('edituserexperience/<int:id>', EditUserExperience),
+    path('deleteuserexperience/<int:id>', DeleteUserExperience),
 
     path('makeuserproject', MakeUserProject),
     path('viewuserproject/<int:id>', ViewUserProject),
+    path('viewsingleproject/<int:id>', ViewSingleProject),
     path('edituserproject/<int:id>', EditUserProject),
     path('updateprojectimage/<int:id>', UpdateProjectImage),
+    path('deleteuserproject/<int:id>', DeleteUserProject),
+
 
     path('makeuserskill', MakeUserSkill),
     path('viewuserskill/<int:id>', ViewUserSkill),
     path('edituserskill/<int:id>', EditUserSkill),
+    path('deleteuserskill/<int:id>', DeleteUserSkill),
 
     path('makeusercertificate', MakeUserCertificate),
     path('viewusercertificate/<int:id>', ViewUserCertificate),
+    path('viewsinglecertificate/<int:id>', ViewSingleCertificate),
     path('editusercertificate/<int:id>', EditUserCertificate),
+    path('deleteusercertificate/<int:id>', DeleteUserCertificate),
+
 
     path('makeuserlink', MakeUserLink),
     path('viewuserlink/<int:id>', ViewUserLink),
